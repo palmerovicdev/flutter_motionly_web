3099f6cabef21e1f7443f0a78621321df4634200
Víctor Manuel Palmero Valdés <palmerodev@gmail.com>
2025-10-11 21:38:09 -0400

    feat: add server and more

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'public-flutter')));
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public-flutter', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
